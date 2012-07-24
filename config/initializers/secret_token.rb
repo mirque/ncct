aa02801ee9861727eab20dce67327e4456a33b7e
David Goldberg <dave@mirque.com>
2012-07-23 20:10:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncct::Application.config.secret_token = 'fefc8b6a5f1893e5f40c408dac691eafe1959a10f4a7121e8ee50ace7e4f07b480c6e09d4bbc23d4f74dcd5f2089756f6830051249bac93eee21ec0a09fdcad7'
